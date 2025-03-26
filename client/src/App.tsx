c466e61c201b22c60b44466a93b8a74104b30445
MrFaiman <57595937+MrFaiman@users.noreply.github.com>
2025-03-26 23:53:21 +0200

    Add buttons and popup dialog to Flight Monitor interface

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,27 @@
 import "./App.css";
-import { AttitudeIndicator, Altimeter, Compass } from "./components";
+import { useState } from "react";
+import { AttitudeIndicator, Altimeter, Compass, Button, Popup } from "./components";
 
 function App() {
+	const [showDialog, setShowDialog] = useState<boolean>(false);
+
 	return (
 		<>
-			<h1>Flight Monitor</h1>
-			<div className="instruments">
-				<Altimeter value={0} />
-				<Compass value={0} />
-				<AttitudeIndicator value={0} />
+			<div className="container">
+				<h1 style={{ textAlign: "center" }}>Flight Monitor</h1>
+				<div className="buttons">
+					<Button>Text</Button>
+					<Button>Visual</Button>
+					<Button onClick={() => setShowDialog(true)}>+</Button>
+				</div>
+				<br />
+				<div className="instruments">
+					<Altimeter value={700} />
+					<Compass value={100} />
+					<AttitudeIndicator value={70} />
+				</div>
 			</div>
+			<Popup show={showDialog} onClose={() => setShowDialog(false)} onSubmit={() => {}} />
 		</>
 	);
 }

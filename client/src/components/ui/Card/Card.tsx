d90861fa55da2cfe36ec7f2f35620fcb213fe683
MrFaiman <57595937+MrFaiman@users.noreply.github.com>
2025-03-27 11:54:02 +0200

    Card align affects title

--- client/src/components/ui/Card/Card.tsx
+++ client/src/components/ui/Card/Card.tsx
@@ -5,7 +5,7 @@ import { CardProps } from "./types";
 const Card: React.FC<CardProps> = ({ title, children, align = "left", ...props }) => {
 	return (
 		<div className={styles.card} {...props}>
-			<div className={styles.cardHeader}>
+			<div className={styles.cardHeader} style={{ justifyContent: align }}>
 				<h3 className={styles.cardTitle}>{title}</h3>
 			</div>
 			<div style={{ textAlign: align }} className={styles.cardContent}>
